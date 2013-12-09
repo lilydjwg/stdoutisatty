746947770b33f3b8ae9a6ce4ac15fb1ca6802d90
lilydjwg <lilydjwg@gmail.com>
2013-12-09 17:21:53 +0800

    optional support for stderr

--- libstdoutisatty.c
+++ libstdoutisatty.c
@@ -1,13 +1,29 @@
 #include<stdarg.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 #include<unistd.h>
 #include<dlfcn.h>
 
 static int (*orig_isatty)(int) = 0;
+static char* env = NULL;
+static int env_len = 0;
+
+static bool should_fake(int fd){
+  if(fd != 1 && fd != 2)
+    return false;
+
+  if(env_len >= 2){
+    return env[fd-1] == 'y';
+  }else if(fd == 1){
+    return true;
+  }
+  return false;
+}
 
 int isatty(int fd){
-  if(fd == 1){
+  if(should_fake(fd)){
     return 1;
   }
   return orig_isatty(fd);
@@ -33,4 +49,8 @@ __attribute__ ((constructor)) static void setup(void) {
   orig_isatty = dlsym(libhdl, "isatty");
   if ((dlerr=dlerror()) != NULL)
     die("Failed to patch isatty() library call: %s", dlerr);
+
+  env = getenv("ISATTY");
+  if(env)
+    env_len = strlen(env);
 }

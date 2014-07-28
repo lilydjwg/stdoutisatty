789748c14fc47b5b58f45cc3730d6a58e2384a62
lilydjwg <lilydjwg@gmail.com>
2014-07-28 16:47:56 +0800

    minor improvement

--- autoclean.sh
+++ autoclean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make distclean
-rm -rf aclocal.m4 config.guess depcomp missing ltmain.sh INSTALL config.sub configure install-sh Makefile.in autom4te.cache/
+rm -rf aclocal.m4 config.guess depcomp compile config.h.in missing ltmain.sh INSTALL config.sub configure install-sh Makefile.in autom4te.cache/
 
--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 aclocal 
+autoheader
 autoconf
 libtoolize --copy
 automake --add-missing --foreign --copy

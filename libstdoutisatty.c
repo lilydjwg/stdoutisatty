bba2256db25e05a5bf3b8dac5a3f0e590bfd0870
SmithGoll <107896294+SmithGoll@users.noreply.github.com>
2024-12-02 00:19:28 +0800

    Improve compatibility
    Make it run on Android and other non-glibc platforms

--- libstdoutisatty.c
+++ libstdoutisatty.c
@@ -29,26 +29,10 @@ int isatty(int fd){
   return orig_isatty(fd);
 }
 
-void die(char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  vfprintf(stderr, fmt, args);
-  va_end(args);
-  fprintf(stderr, "\n");
-  fflush(stderr);
-  exit(-1);
-}
-
 __attribute__ ((constructor)) static void setup(void) {
   void *libhdl;
-  char *dlerr;
 
-  if (!(libhdl=dlopen("libc.so.6", RTLD_LAZY)))
-    die("Failed to patch library calls: %s", dlerror());
-
-  orig_isatty = dlsym(libhdl, "isatty");
-  if ((dlerr=dlerror()) != NULL)
-    die("Failed to patch isatty() library call: %s", dlerr);
+  orig_isatty = dlsym(RTLD_NEXT, "isatty");
 
   env = getenv("ISATTY");
   if(env)
